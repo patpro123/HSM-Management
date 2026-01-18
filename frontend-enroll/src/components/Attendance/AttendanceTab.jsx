275f85109219936fb0a78ab413eb3bbde3abda94
Parthoprotim Mukherjee <srinikamukherjee@Srinikas-MacBook-Air.local>
2026-01-18 10:46:23 +0530

    Fix AttendanceTab: add missing API_BASE_URL import

--- frontend-enroll/src/components/Attendance/AttendanceTab.jsx
+++ frontend-enroll/src/components/Attendance/AttendanceTab.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { API_BASE_URL } from '../../config'
 import BatchSelector from './BatchSelector'
 import AttendanceList from './AttendanceList'
 import './AttendanceTab.css'
